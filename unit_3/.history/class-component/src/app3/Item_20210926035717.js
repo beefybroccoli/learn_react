630547ab6e47d5560fd4e0399a836bc155ae0f71
beefybroccoli <tedkimemail@yahoo.com>
2021-09-26 04:05:26 -0700

    added unit 3

--- unit_3/.history/class-component/src/app3/Item_20210926035717.js
+++ unit_3/.history/class-component/src/app3/Item_20210926035717.js
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
+
+
+export default class Item extends React.Component {
+    const Container = styled.div`
   border: 1px solid green;
   margin: 5px;
   padding: 5px;
-  /* text-decoration: ${(props) => (props.purchased ? "line-through" : "")};
-  background-color: ${(props) => (props.purchased ? "green" : "yellow")}; */
+  text-decoration: ${(props) => (props.purchased ? "line-through" : "")};
+  background-color: ${(props) => (props.purchased ? "green" : "yellow")};
 `;
-
-export default class Item extends React.Component {
   handleChanges = (e) => {};
   render() {
     const { name, purchased } = this.props;
