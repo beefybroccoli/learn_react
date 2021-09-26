b7ed3604e5d2c9787634bbddce4651ea9cf36b0d
beefybroccoli <tedkimemail@yahoo.com>
2021-09-26 02:55:35 -0700

    added unit 3

--- unit_3/.history/class-component/src/index_20210926025450.js
+++ unit_3/.history/class-component/src/index_20210926025450.js
@@ -7,7 +7,6 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
     <App2 favorite="(no book title)" />
   </React.StrictMode>,
   document.getElementById("root")
